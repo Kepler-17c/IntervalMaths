46a4d63b5db23f89fa0d5585194182c32ebee5ab
Kepler-17c <17955785+Kepler-17c@users.noreply.github.com>
2024-07-17 15:43:50 +0200

    Implemented interface Comparable

--- src/main/java/space/kepler_17c/interval/Rational.java
+++ src/main/java/space/kepler_17c/interval/Rational.java
@@ -1,8 +1,9 @@
 package space.kepler_17c.interval;
 
 import java.math.BigInteger;
+import java.util.Objects;
 
-public class Rational {
+public class Rational implements Comparable<Rational> {
     public static final Rational ZERO = new Rational(BigInteger.ZERO, BigInteger.ONE);
     public static final Rational ONE = new Rational(BigInteger.ONE, BigInteger.ONE);
     public static final Rational TWO = new Rational(BigInteger.TWO, BigInteger.ONE);
@@ -19,12 +20,82 @@ public class Rational {
         this.denominator = denominator;
     }
 
+    public boolean isFinite() {
+        return !denominator.equals(BigInteger.ZERO);
+    }
+
+    public boolean isInfinite() {
+        return !numerator.equals(BigInteger.ZERO) && denominator.equals(BigInteger.ZERO);
+    }
+
+    public boolean isNan() {
+        return numerator.equals(BigInteger.ZERO) && denominator.equals(BigInteger.ZERO);
+    }
+
+    public int signum() {
+        return numerator.signum();
+    }
+
+    public Rational negate() {
+        return new Rational(numerator.negate(), denominator);
+    }
+
+    @Override
+    public int compareTo(Rational other) {
+        if (isFinite() && other.isFinite()) {
+            BigInteger thisNum = numerator.multiply(other.denominator);
+            BigInteger otherNum = other.numerator.multiply(denominator);
+            return thisNum.compareTo(otherNum);
+        }
+        // only combinations with NaN or infinity remain
+        // first, deal with NaN
+        if (isNan() && other.isNan()) {
+            return 0;
+        }
+        if (isNan()) {
+            return -1;
+        }
+        if (other.isNan()) {
+            return 1;
+        }
+        // only combinations of finite and infinity remain
+        if (isInfinite() && other.isInfinite()) {
+            return numerator.compareTo(other.numerator);
+        }
+        // only one is infinite, the other one is finite
+        return isInfinite() ? signum() : -other.signum();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Rational r) {
+            return this.numerator.equals(r.numerator) && this.denominator.equals(r.denominator);
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(numerator, denominator);
+    }
+
+    @Override
+    public String toString() {
+        if (isFinite()) {
+            return numerator + "/" + denominator;
+        }
+        if (isInfinite()) {
+            return signum() > 0 ? "Infinity" : "-Infinity";
+        }
+        return "NaN";
+    }
+
     private Rational normaliseFraction() {
-        if (numerator.equals(BigInteger.ZERO) && denominator.equals(BigInteger.ZERO)) {
+        if (isNan()) {
             return this;
         } else if (numerator.equals(BigInteger.ZERO)) {
             return ZERO;
-        } else if (denominator.equals(BigInteger.ZERO)) {
+        } else if (isInfinite()) {
             return numerator.signum() > 0 ? POSITIVE_INFINITY : NEGATIVE_INFINITY;
         } else {
             boolean negative = false;
--- src/test/java/space/kepler_17c/interval/RationalTest.java
+++ src/test/java/space/kepler_17c/interval/RationalTest.java
@@ -2,6 +2,7 @@ package space.kepler_17c.interval;
 
 import java.lang.reflect.Field;
 import java.math.BigInteger;
+import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -18,7 +19,80 @@ class RationalTest {
     }
 
     @Test
-    public void cancelFractionTest() {
+    public void comparisonsTest() {
+        // zero
+        Assertions.assertTrue(Rational.ZERO.isFinite());
+        Assertions.assertFalse(Rational.ZERO.isInfinite());
+        Assertions.assertFalse(Rational.ZERO.isNan());
+        // one
+        Assertions.assertTrue(Rational.ONE.isFinite());
+        Assertions.assertFalse(Rational.ONE.isInfinite());
+        Assertions.assertFalse(Rational.ONE.isNan());
+        // +Inf
+        Assertions.assertFalse(Rational.POSITIVE_INFINITY.isFinite());
+        Assertions.assertTrue(Rational.POSITIVE_INFINITY.isInfinite());
+        Assertions.assertFalse(Rational.POSITIVE_INFINITY.isNan());
+        // -Inf
+        Assertions.assertFalse(Rational.NEGATIVE_INFINITY.isFinite());
+        Assertions.assertTrue(Rational.NEGATIVE_INFINITY.isInfinite());
+        Assertions.assertFalse(Rational.NEGATIVE_INFINITY.isNan());
+        // NaN
+        Assertions.assertFalse(Rational.NAN.isFinite());
+        Assertions.assertFalse(Rational.NAN.isInfinite());
+        Assertions.assertTrue(Rational.NAN.isNan());
+
+        // equals
+        Assertions.assertEquals(Rational.valueOf(4, 4), Rational.ONE);
+        Assertions.assertEquals(Rational.valueOf(1, -1), Rational.ONE.negate());
+        Assertions.assertEquals(Rational.valueOf(2, 0), Rational.POSITIVE_INFINITY);
+        Assertions.assertEquals(Rational.valueOf(-2, 0), Rational.NEGATIVE_INFINITY);
+        Assertions.assertEquals(Rational.valueOf(0, 0), Rational.NAN);
+        Assertions.assertEquals(Rational.valueOf(8, 4), Rational.TWO);
+        Assertions.assertEquals(Rational.valueOf(100, 10), Rational.TEN);
+
+        // compareTo
+        List<Rational> orderedTestValues = List.of(
+                Rational.NAN,
+                Rational.NEGATIVE_INFINITY,
+                Rational.TEN.negate(),
+                Rational.TWO.negate(),
+                Rational.ONE.negate(),
+                Rational.valueOf(-1, 5),
+                Rational.valueOf(-1, 10),
+                Rational.ZERO,
+                Rational.valueOf(1, 4),
+                Rational.valueOf(3, 8),
+                Rational.valueOf(3, 7),
+                Rational.ONE,
+                Rational.TWO,
+                Rational.TEN,
+                Rational.POSITIVE_INFINITY);
+        for (int i = 0; i < orderedTestValues.size(); i++) {
+            Rational rational = orderedTestValues.get(i);
+            orderedTestValues.subList(0, i).forEach(r -> {
+                Assertions.assertEquals(1, rational.compareTo(r), "Comparing " + rational + " to " + r);
+                Assertions.assertEquals(-1, r.compareTo(rational), "Comparing " + r + " to " + rational);
+            });
+            orderedTestValues.subList(i + 1, orderedTestValues.size()).forEach(r -> {
+                Assertions.assertEquals(-1, rational.compareTo(r), "Comparing " + rational + " to " + r);
+                Assertions.assertEquals(1, r.compareTo(rational), "Comparing " + r + " to " + rational);
+            });
+        }
+    }
+
+    @Test
+    public void toStringTest() {
+        Assertions.assertEquals("0/1", Rational.ZERO.toString());
+        Assertions.assertEquals("1/1", Rational.ONE.toString());
+        Assertions.assertEquals("2/1", Rational.TWO.toString());
+        Assertions.assertEquals("10/1", Rational.TEN.toString());
+        Assertions.assertEquals("Infinity", Rational.POSITIVE_INFINITY.toString());
+        Assertions.assertEquals("-Infinity", Rational.NEGATIVE_INFINITY.toString());
+        Assertions.assertEquals("NaN", Rational.NAN.toString());
+    }
+
+    @Test
+    public void normaliseFractionTest() {
         inspectRational(1, 2, Rational.valueOf(2, 4));
         inspectRational(-1, 2, Rational.valueOf(2, -4));
         inspectRational(-1, 2, Rational.valueOf(-2, 4));
